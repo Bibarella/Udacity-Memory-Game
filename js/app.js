f63a6db6bda592846c1af1678a20fa914b9ffc3f
Bianca Glasner <neotrinity1501@yahoo.de>
2019-01-27 22:45:06 +0100

    bug fixes

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 /*
  * Create a list that holds all of your cards
  */ 
-const deck = document.querySelector('.deck');
 let toggledCards = [];
 let moves = 0;
 let clockOff = true;
 let time = 0;
 let clockId;
 let matched =0;
+const TOTAL_PAIRS = 8;
+resetCards();
+
+const deck = document.querySelector('.deck');
 
 function shuffleDeck() {
 	const cardsToShuffle = Array.from(document.querySelectorAll('.deck li'));
@@ -67,7 +70,7 @@ function shuffle(array) {
 				checkScore();
 		}
 	}	
-});
+})
 
 function isClickValid(clickTarget) {
 	return (
@@ -85,20 +88,21 @@ function toggleCard(card) {
 
 function addToggleCard(clickTarget) {
 	toggledCards.push(clickTarget);
-	console.log(toggledCards);
 }
 
 function checkForMatch() {
 	if (
 		toggledCards[0].firstElementChild.className ===
-		toggledCards[1].firstElementChild.className
-	) {
+		toggledCards[1].firstElementChild.className) {
 		toggledCards[0].classList.toggle('match');
 		toggledCards[1].classList.toggle('match');
 		toggledCards = [];
 		matched++;
+		if (matched === TOTAL_PAIRS) {
+		gameOver();
+		}
 	} else {
-		setTimeout(() => {
+		setTimeout(function() {
 			toggleCard(toggledCards[0]);
 			toggleCard(toggledCards[1]);
 			toggledCards = [];
@@ -108,13 +112,13 @@ function checkForMatch() {
 
 function addMove() {
 	moves++;
-	const movesText = document.querySelector('.moves');
+	let movesText = document.querySelector('.moves');
 	movesText.innerHTML = moves;
 }
 
 function checkScore() {
-	if (moves === 16 || moves === 24
-	){	removeStar();
+	if (moves === 12 || moves === 24
+	){	hideStar();
 	}
 }
 	
@@ -127,11 +131,9 @@ function hideStar() {
 		}
 	}
 }
-hideStar();
-hideStar();
 
 function startClock() {
-	clockId = setInterval(() => {
+	clockId = setInterval(function() {
 		time++;
 		displayTime();
 		console.log(time);
@@ -143,13 +145,12 @@ function displayTime() {
 	console.log(clock);
 	clock.innerHTML = time;
 	const minutes = Math.floor(time / 60);
-	const seconds = Math.floor(time % 60);
+	const seconds = time % 60;
 	if (seconds < 10) {
 		clock.innerHTML = `${minutes}:0${seconds}`;
 	} else {
 		clock.innerHTML = `${minutes}:${seconds}`;
 	}
-	console.log(clock);
 }	
 
 function stopClock() {
@@ -159,25 +160,20 @@ function stopClock() {
 function toggleModal() {
 	const modal = document.querySelector('.modal__background');
 	modal.classList.toggle('hide');
-	modal.classList.toggle('show');
+	/*modal.classList.toggle('show'); */
 }
-
-toggleModal()
-toggleModal()
 
 function writeModalStats() {
 	const timeStat = document.querySelector('.modal__time');
 	const clockTime = document.querySelector('.clock').innerHTML;
-	const moveStat = document.querySelector('.modal__moves');
+	const movesStat = document.querySelector('.modal__moves');
+	const starsStat = document.querySelector('.modal__stars')
 	const stars = getStars();
 	
 	timeStat.innerHTML = `Time = ${clockTime}`;
 	movesStat.innerHTML = `Moves = ${moves}`; 
 	starsStat.innerHTML = `Stars = ${stars}`;
 } 
-
-writeModalStats();
-toggleModal();
 
 function getStars() {
 	stars = document.querySelectorAll('.stars li');
@@ -187,22 +183,24 @@ function getStars() {
 			starCount++;
 		}
 	}
-	console.log(starCount);
 	return starCount;
 }
 
-document.querySelector('.modal__cancel').addEventListener('click', () => {
-	toggleModal('hide');
+document.querySelector('.modal__cancel').addEventListener('click', function() {
+    toggleModal();
 });
 
-document.querySelector('.modal__replay').addEventListener('click', () => {
-	console.log('replay');
-});
+document.querySelector('.modal__replay').addEventListener('click', replayGame);
+
+document.querySelector('.restart').addEventListener('click', resetGame);
 
 function resetGame() {
+	matched = 0;
+	toggleCards = [];
 	resetClockAndTime();
 	resetMoves();
 	resetStars();
+	resetCards();
 	shuffleDeck();
 }
 
@@ -225,15 +223,6 @@ function resetStars() {
 		star.style.display = 'inline';
 	}
 }
-
-document.querySelector('.restart').addEventListener('click', resetGame);
-
-document.querySelector('.modal__replay').addEventListener('click', replayGame);
-
-const TOTAL_PAIRS = 8;
-	if (matched === TOTAL_PAIRS) {
-		gameOver();
-	}
 	
 function gameOver() {
 	stopClock();
@@ -242,8 +231,10 @@ function gameOver() {
 }
 
 function replayGame() {
+	matched = 0;
 	resetGame();
 	toggleModal();
+	resetCards();
 }
 
 function resetCards() {
